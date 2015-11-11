285109cc9df698f1bcb09d64a18d614fdd9b7cd0
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-11-11 17:09:02 -0600

    Added functional tree structure

--- Tree/tree.py
+++ Tree/tree.py
@@ -1,40 +1,90 @@
+import sys
+
+#class Node(object) :
+  #def __init__(self,nodes=None) :
+    #self.nodes = dict(nodes) if nodes else {}
+    #self.numbers = {}
 
 
 class Tree(object) :
-  def __init__(self,trees=None) :
-    self.trees = dict(trees) if trees else {}
+  def __init__(self) :
+    #self.trees = dict(trees) if trees else {}
+    self.trees = {}
+    self.number = 0
     
-  def insert_subtree(self, name, tree) :
-    if name in self.trees :
-      raise TreeAlreadyExists()
-    
-    self.trees[name] = tree
-    
-    
-  def get(self, *args) :
-    child_name, rest = args[0], args[1:]
-    child = self._get_child(child_name)
-    
-    if len(rest) :
-      return child.get(*rest)
+  def insert_word(self,word) :
+    if word :
+      if word[0] not in self.trees :
+        tree1 = Tree()
+        self.trees[word[0]] = tree1
+
+      self.trees[word[0]].insert_word(word[1:])
     else :
-      return [item for item in child]
+      self.number += 1
+
+
+  def word_count(self,word) :
+    if word  :
+      try :
+        return self.trees[word[0]].word_count(word[1:])
+      except :
+	print "No word in tree " + word
+
+    else :
+      return self.number
+      
+  def traverse(self,word) :
+    if word :
+      #try :
+      return [word[0] +  i for i in self.trees[word[0]].traverse(word[1:])]
+      #except : 
+        #print "Cant tranverse using " + word 
+    else :
+      if self.trees and self.number == 0:
+	l=[]
+      else :
+	l= ["-"+str(self.number)]
+      for k,v in self.trees.iteritems() :
+	l+=[k + i for i in v.traverse(word)]
+      return l      
+      #return [k + i for i in  v.traverse(word)  for k,v in self.trees.iteritems()]
+    
+    
+      
+
+  #def insert_subtree(self, name, tree) :
+    #if name in self.trees :
+      #raise TreeAlreadyExists()
+    
+    #self.trees[name] = tree
+    #self.numbers[name] = 1    
+    
+  #def get(self, *args) :
+    #child_name, rest = args[0], args[1:]
+    #child = self._get_child(child_name)
+    
+    #if len(rest) :
+      #return child.get(*rest)
+    #else :
+      #return [item for item in child]
     
   
-  def _get_child(self, name) :
-    if name not in self.trees:
-      raise KeyError("Child %s does not exist" % name)
-    return self.trees[name]
+  #def _get_child(self, name) :
+    #if name not in self.trees:
+      #raise KeyError("Child %s does not exist" % name)
+    #return self.trees[name]
 
-  def insert(self, *args):
-    child_name, rest = args[0], args[1:]
-    child = self._get_child(child_name)
-    child.insert(*rest)
 
-  def __iter__(self):
-    for key in sorted(self.trees.keys()):
-       for item in self.trees[key]:
-         yield item
 
-class TreeAlreadyExists(Exception):
-    pass
+  #def insert(self, *args):
+    #child_name, rest = args[0], args[1:]
+    #child = self._get_child(child_name)
+    #child.insert(*rest)
+
+  #def __iter__(self):
+    #for key in sorted(self.trees.keys()):
+      #for item in self.trees[key]:
+	#yield item
+
+#class TreeAlreadyExists(Exception):
+    #pass
---
+++ Tree/tree_test.py
@@ -0,0 +1,43 @@
+import tree as tr
+import unittest
+
+t=tr.Tree()
+list_words =["test","command","cat","caterpillar","chown","chmod","chroot","table","words","word","word","word","wordd"]
+for i in list_words :
+  t.insert_word(i)
+
+
+class TestTree(unittest.TestCase) :
+  #def test_create_tree(self) :
+    #self.failUnlessRaises(ValueError,tr.Tree)
+      
+  def test_count(self) :
+    self.assertEqual(t.word_count("word"),3)
+    
+  def test_traverse_1(self) :
+    self.assertItemsEqual(t.traverse("t"),['test-1','table-1']) 
+    
+  def test_traverse_2(self) :
+    self.assertItemsEqual(t.traverse("ch"),['chown-1','chmod-1','chroot-1'])
+  
+  def test_traverse_3(self) :
+    self.assertItemsEqual(t.traverse("w"),['word-3','words-1','wordd-1'])  
+    
+  
+  
+  #def test_count(self) :
+    #self.assertEqual(t.word)
+
+
+#print "TEST 1 : ", t.word_count("word")
+
+#print "TEST 2 : ", t.traverse("wu")
+
+#print "TEST 3 : ", t.word_count("letra")
+
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+      
